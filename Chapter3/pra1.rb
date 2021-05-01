1ad68c8440a521b05fb7bcdbe9a64334cc69ee3f
TakehiroKatayama <t.katayama.info@gmail.com>
2021-05-01 14:42:59 +0000

    lesson2

---
+++ Chapter3/pra1.rb
@@ -0,0 +1,20 @@
+a = 2
+p a < 365
+
+p a == 1 + 1
+
+season = "春"
+unless season == "夏"
+  puts "あんまん食べたい"
+end
+
+season = "春"
+if season != "夏"
+  puts "あんまん食べたい"
+end
+
+season = "夏"
+if season == "夏"
+  puts "かき氷たべたい"
+  puts "麦茶のみたい"
+end
